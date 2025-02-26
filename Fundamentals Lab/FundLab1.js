a1dd50c23110688118b73e84eb13a69f4aa5485a
Will-remote <wtbeaven@gmail.com>
2025-02-26 21:12:24 +1100

    Finished fundamentals exercise 1

---
+++ Fundamentals Lab/FundLab1.js
@@ -0,0 +1,19 @@
+"" + 1 + 0 // 10 
+"" - 1 + 0 // -1
+true + false // 1
+!true // false
+6 / "3" // 2
+"2" * "3" // 6
+4 + 5 + "px" // 9px
+"$" + 4 + 5 // $45
+"4" - 2 // 2
+"4px" - 2 // NaN
+" -9 " + 5 //  -9 5
+" -9 " - 5 // -14
+null + 1 // 1
+undefined + 1 // 1
+undefined == null // true
+undefined === null // false
+" \t \n" - 2 // -2
+
+console.log(" \t \n" - 2)
