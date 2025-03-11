c50a14bb2b46530f59b71317e8549270c299d2a0
Will-remote <wtbeaven@gmail.com>
2025-03-11 18:49:56 +1100

    Finished int. lab 3

--- Intermediate Lab/Exercise3.js
+++ Intermediate Lab/Exercise3.js
@@ -1,4 +1,6 @@
 // 3.
+// Use the following animals array for the below tasks. Test each one by printing the result to
+// the console.
 // a) Add 2 new values to the end
 // b) Add 2 new values to the beginning
 // c) Sort the values alphabetically
@@ -9,6 +11,22 @@
 // regardless of upper/lower case.
 
 const animals = ["Tiger", "Giraffe"];
-animals.push("Elephant", "Pangolin");
-animals.unshift("Lion", "Zebra");
+animals.push("Elephant", "Whale", "Pangolin");
+animals.unshift("Zebra");
+animals.sort();
+
+function findMatchingAnimals(beginsWith) {
+  return animals.filter(
+    (word) => word.charAt(0) == String(beginsWith).toUpperCase()
+  );
+}
+
+function replaceMiddleAnimal(newValue) {
+    middle = Math.round(animals.length/2) -1;
+    animals.splice(middle, 1, newValue);
+}
+
+console.log(animals);
+console.log(findMatchingAnimals("t"));
+replaceMiddleAnimal("Magical Beast");
 console.log(animals);
