8e9509f28f1dae2d8865966b8a127def525bfc0b
Will <wtbeaven@gmail.com>
2025-03-17 17:55:53 +1100

    Worked on lab 7

---
+++ Intermediate Lab/Exercise7.js
@@ -0,0 +1,43 @@
+// 7. Use the following array of book objects to practice the array functions for map, find and
+// filter. Test each of your answers to the below tasks.
+
+const books = [
+  {
+    id: 1,
+    title: "The Great Gatsby",
+    author: "F. Scott Fitzgerald",
+    year: 1925,
+  },
+  { id: 2, title: "To Kill a Mockingbird", author: "Harper Lee", year: 1960 },
+  { id: 3, title: "1984", author: "George Orwell", year: 1949 },
+  { id: 4, title: "Brave New World", author: "Aldous Huxley", year: 1932 },
+  {
+    id: 5,
+    title: "The Catcher in the Rye",
+    author: "J.D. Salinger",
+    year: 1951,
+  },
+];
+
+// a) Write a function getBookTitle(bookId) that uses the find function to return the
+// title of the book object with the matching id.
+// b) Write a function getOldBooks() that uses the filter function to return all book
+// objects written before 1950.
+// c) Write a function addGenre() that uses the map function to add a new genre property
+// to all of the above books, with the value ‘classic’.
+// d) (Extension) Write a function getTitles(authorInitial) that uses map and
+// filter together to return an array of book titles for books written by authors whose
+// names start with authorInitial.
+// e) (Extension) Write a function latestBook() that uses find and forEach to get the
+// book with the most recent publication date.
+
+function getBookTitle(bookId) {
+  return books.find(({ id }) => id == bookId);
+}
+console.log(getBookTitle(3));
+
+function getOldBooks() {
+  let result = books.filter(year < 1950);
+  return result;
+}
+console.log(result);
